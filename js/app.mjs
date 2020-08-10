681423fed9d5a7f4d9f69e9771ee63c43aaf2de0
Hemal Gokhale <31528745+gokhalehemal11@users.noreply.github.com>
2020-08-10 09:47:13 +0530

    Add files via upload

---
+++ js/app.mjs
@@ -0,0 +1,205 @@
+import { addFontFromFile, formatText } from './utils/helpers.mjs';
+import { generateImages, downloadAsPDF } from './generate-images.mjs';
+import { setInkColor, toggleDrawCanvas } from './utils/draw.mjs';
+
+/**
+ *
+ * Hi there! This is the entry file of the tool and deals with adding event listeners
+ * and some other functions.
+ *
+ * To contribute, you can follow the imports above and make changes in the file
+ * related to the issue you've choosen.
+ *
+ * If you have any questions related to code, you can drop them in my Twitter DM @saurabhcodes
+ * or in my email at saurabhdaware99@gmail.com
+ *
+ * Thanks! and Happy coding 🌻
+ *
+ */
+
+const pageEl = document.querySelector('.page-a');
+const margin_top= document.querySelector('.margined .top-margin');
+const margin_left= document.querySelector('.margined .left-margin');
+
+const setMarginLeftColor= (attrib, v) => (margin_left.style[attrib] = v);
+const setMarginTopColor= (attrib, v) => (margin_top.style[attrib] = v);
+const setTextareaStyle = (attrib, v) => (pageEl.style[attrib] = v);
+
+/**
+ * Add event listeners here, they will be automatically mapped with addEventListener later
+ */
+const EVENT_MAP = {
+  '#generate-image-form': {
+    on: 'submit',
+    action: (e) => {
+      e.preventDefault();
+      generateImages();
+    }
+  },
+  '#handwriting-font': {
+    on: 'change',
+    action: (e) => {
+      //console.log(e.target.value);
+      document.body.style.setProperty('--handwriting-font', e.target.value)
+    }
+  },
+  '#font-size': {
+    on: 'change',
+    action: (e) => {
+      //console.log(e.target.value);
+      setTextareaStyle('fontSize', e.target.value + 'pt')
+    }
+  },
+/*  '#letter-spacing': {
+    on: 'change',
+    action: (e) => setTextareaStyle('letterSpacing', e.target.value + 'pt')
+  },*/
+  '#word-spacing': {
+    on: 'change',
+    action: (e) => setTextareaStyle('wordSpacing', e.target.value + 'px')
+  },
+/*  '#top-padding': {
+    on: 'change',
+    action: (e) => {
+      document.querySelector('.page-a .paper-content').style.paddingTop =
+        e.target.value + 'px';
+    }
+  },*/
+  '#font-file': {
+    on: 'change',
+    action: (e) => addFontFromFile(e.target.files[0])
+  },
+  '#ink-color': {
+    on: 'change',
+    action: (e) => {
+      document.body.style.setProperty('--ink-color', e.target.value);
+      setInkColor(e.target.value);
+    }
+  },
+  '#page-format': {
+    on: 'change',
+    action: (e) => {
+      if(e.target.value == "default"){
+        pageEl.classList.add('margined');
+        pageEl.classList.add('lines');
+      }
+      else if (e.target.value == "no-margin"){
+        pageEl.classList.add('lines');
+        pageEl.classList.remove('margined');
+      }
+      else if(e.target.value == 'blank-white'){
+        pageEl.classList.remove('lines');
+        pageEl.classList.remove('margined');
+      }
+    }
+  },
+  '#page-margin': {
+    on: 'change',
+    action: (e) => {
+      //console.log(e.target.value);
+      //document.body.style.setProperty('--page-margin-color', e.target.value);
+      setMarginTopColor('border-bottom', e.target.value);
+      setMarginLeftColor('border-right', e.target.value);
+    }
+  },
+ /* '#paper-margin-toggle': {
+    on: 'change',
+    action: () => {
+      if (pageEl.classList.contains('margined')) {
+        pageEl.classList.remove('margined');
+      } else {
+        pageEl.classList.add('margined');
+      }
+    }
+  },
+  '#paper-line-toggle': {
+    on: 'change',
+    action: () => {
+      if (pageEl.classList.contains('lines')) {
+        pageEl.classList.remove('lines');
+      } else {
+        pageEl.classList.add('lines');
+      }
+    }
+  },
+  '#draw-diagram-button': {
+    on: 'click',
+    action: () => {
+      toggleDrawCanvas();
+    }
+  },
+  '.draw-container .close-button': {
+    on: 'click',
+    action: () => {
+      toggleDrawCanvas();
+    }
+  },*/
+  '#download-as-pdf-button': {
+    on: 'click',
+    action: () => {
+      downloadAsPDF();
+    }
+  },
+  '.page-a .paper-content': {
+    on: 'paste',
+    action: formatText
+  }
+};
+
+for (const eventSelector in EVENT_MAP) {
+  console.log(eventSelector)
+  document
+    .querySelector(eventSelector)
+    .addEventListener(
+      EVENT_MAP[eventSelector].on,
+      EVENT_MAP[eventSelector].action
+    );
+}
+
+/**
+ * This makes toggles, accessible.
+ */
+document.querySelectorAll('.switch-toggle input').forEach((toggleInput) => {
+  toggleInput.addEventListener('change', (e) => {
+    if (toggleInput.checked) {
+      document.querySelector(
+        `label[for="${toggleInput.id}"] .status`
+      ).textContent = 'on';
+      toggleInput.setAttribute('aria-checked', true);
+    } else {
+      toggleInput.setAttribute('aria-checked', false);
+      document.querySelector(
+        `label[for="${toggleInput.id}"] .status`
+      ).textContent = 'off';
+    }
+  });
+});
+
+/**
+ * Set GitHub Contributors
+ */
+
+/*fetch(
+  'https://api.github.com/repos/saurabhdaware/text-to-handwriting/contributors'
+)
+  .then((res) => res.json())
+  .then((res) => {
+    document.querySelector('#project-contributors').innerHTML = res
+      .map(
+        (contributor) => `
+        <div class="contributor-profile shadow">
+          <a href="${contributor.html_url}">
+            <img 
+              alt="GitHub avatar of contributor ${contributor.login}" 
+              class="contributor-avatar" 
+              loading="lazy" 
+              src="${contributor.avatar_url}" 
+            />
+            <div class="contributor-username">${contributor.login}</div>
+          </a>
+        </div>
+      `
+      )
+      .join('');
+  });*
+*/
